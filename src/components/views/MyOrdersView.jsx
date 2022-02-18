48a24d9327fcf86b07a0c6f6829423b858dadc40
Your NameAymanMansoor <you@example.com>
2022-02-17 23:53:03 -0500

    MyOrdersView_pullrequest

--- src/components/views/MyOrdersView.jsx
+++ src/components/views/MyOrdersView.jsx
@@ -3,6 +3,5 @@ import React from "react";
 export const MyOrdersView = () => {
   return <div>My Orders View</div>;
 };
-
 // everything displayed has to be between the <div> 
 // focus on grids material ui websit for grid or just google "how to do ___ in react"
